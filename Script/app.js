2dba524070bf2505784530a344e1357395981196
Sebastian Matthews <admiraloven@gmail.com>
2017-03-06 21:21:56 -0500

    finished setting up and started learning, just saving progress

--- Script/app.js
+++ Script/app.js
@@ -1,1 +1,36 @@
-var app = angular.module('store', [ ]);
+(function(){
+
+
+	var app = angular.module('store', [ ]);
+
+	app.controller("StoreController", function(){
+		this.products = gems;
+	})
+
+	var gems= [
+	{
+		name: "Dodecahedron",
+		price: 2,
+		description: "nope",
+		canPurchase: true,
+		images: [
+			{
+				full: "Assets/diamond.png"
+			}
+		]
+	},
+
+	{
+		name: "Pentagonal Gem",
+		price: 5.95,
+		description: "maybe",
+		canPurchase: true,
+		images: [
+			{
+				full: "Assets/emerald.png"
+			}
+		]
+	}
+
+	]
+})();
