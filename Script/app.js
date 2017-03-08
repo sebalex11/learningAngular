5a08bc34ebcfaed6587dca35935274d9e34a73e9
Sebastian Matthews <admiraloven@gmail.com>
2017-03-08 17:03:26 -0500

    finished up the tutorial and it is fully functional

--- Script/app.js
+++ Script/app.js
@@ -1,65 +1,18 @@
 (function(){
 
 
-	var app = angular.module('store', [ ]);
+	var app = angular.module('store', ["store-products"]);
 
-	app.controller("StoreController", function(){
-		this.products = gems;
-	})
+	app.controller("StoreController", ["$http", function($http){
+		var store = this;
 
-	var gems= [
-	{
-		name: "Dodecahedron",
-		price: 2,
-		description: "this is actually a diamond you fool!",
-		canPurchase: true,
-		images: [
-			{
-				full: "Assets/diamond.png"
-			}],
-		reviews: [
-			{
-				stars: 5,
-				body: "I love this product",
-				author: "nobody@nothing.com",
-			},
-			{
-				stars: 1,
-				body: "I hate this thing",
-				author: "trash@terrible.com",
-			}
-			],
+		store.products = [];
 
-	},
-
-	{
-		name: "Pentagonal Gem",
-		price: 5.95,
-		description: "This is most definitely not a Pentagonal gem, it has at least 6 sides",
-		canPurchase: true,
-		images: [
-			{
-				full: "Assets/emerald.png"
-			}
-		]
-	}
-
-	];
-
-
-
-
-
-	// app.controller("PanelController", function(){
-	// 	this.tab = 1;
-	// 	this.selectTab = function(setTab){
-	// 		this.tab = setTab;
-	// 	};
-
-	// 	this.isSelected = function(checkTab){
-	// 		return this.tab === checkTab;
-	// 	};
-	// });
+		$http.get("products.json").then(function(data){
+			store.products = data.data.gems;
+			console.log(store.products)
+		});
+	}]);
 
 	app.controller("ReviewController", function(){
 		this.review = {};
@@ -67,30 +20,6 @@
 			product.reviews.push(this.review);
 			this.review = {};
 		}
-	});
-
-	app.directive('productTitle', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'product-title.html'
-		};
-	});
-
-	app.directive('productPanels', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-			controller: function(){
-				this.tab = 1;
-				this.selectTab = function(setTab){
-					this.tab = setTab;
-				};
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;
-				};
-			},
-			controllerAs: "panel"
-		};
 	});
 
 
